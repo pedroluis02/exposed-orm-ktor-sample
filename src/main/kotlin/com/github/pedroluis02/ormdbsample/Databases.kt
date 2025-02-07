876c1ba2299beb81997876e30b2d3ef992d0cb33
Pedro Luis <pedroluisnr22@gmail.com>
2025-02-06 21:58:25 -0500

    feat: :sparkles: add router and use invalid id execption

--- src/main/kotlin/com/github/pedroluis02/ormdbsample/Databases.kt
+++ src/main/kotlin/com/github/pedroluis02/ormdbsample/Databases.kt
@@ -15,39 +15,41 @@ fun Application.configureDatabases() {
         password = "",
     )
     val userService = UserService(database)
-    
-    routing {
-        // Create user
-        post("/users") {
-            val user = call.receive<ExposedUser>()
-            val id = userService.create(user)
-            call.respond(HttpStatusCode.Created, id)
-        }
 
-        // Read user
-        get("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = userService.read(id)
-            if (user != null) {
-                call.respond(HttpStatusCode.OK, user)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
+    routing {
+        route("/users") {
+            post {
+                val user = call.receive<ExposedUser>()
+                val id = userService.create(user)
+                call.respond(HttpStatusCode.Created, id)
+            }
+
+            get("/{id}") {
+                val id = call.extractIDOrThrow()
+                val user = userService.read(id)
+                if (user != null) {
+                    call.respond(HttpStatusCode.OK, user)
+                } else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            }
+
+            put("/{id}") {
+                val id = call.extractIDOrThrow()
+                val user = call.receive<ExposedUser>()
+                userService.update(id, user)
+                call.respond(HttpStatusCode.OK)
+            }
+
+            delete("/{id}") {
+                val id = call.extractIDOrThrow()
+                userService.delete(id)
+                call.respond(HttpStatusCode.OK)
             }
         }
-
-        // Update user
-        put("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            val user = call.receive<ExposedUser>()
-            userService.update(id, user)
-            call.respond(HttpStatusCode.OK)
-        }
-
-        // Delete user
-        delete("/users/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
-            userService.delete(id)
-            call.respond(HttpStatusCode.OK)
-        }
     }
+}
+
+private fun RoutingCall.extractIDOrThrow(): Int {
+    return parameters["id"]?.toInt() ?: throw InvalidIDArgumentException()
 }
